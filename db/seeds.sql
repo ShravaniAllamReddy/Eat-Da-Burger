74364fc3865f536a6257cd5b4890d294e45a4c90
Shravani Allam Reddy <shravani.allam@gmail.com>
2020-11-08 22:19:06 -0700

    modified table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (name) VALUES ('Chicken Burger');
-INSERT INTO burgers (name) VALUES ('Plant based burger');
+INSERT INTO burgers (name, devour) VALUES ('Plant based burger', true);
 INSERT INTO burgers (name) VALUES ('Salmon burger');
