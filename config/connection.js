457c6570ac7b557c29142f0e283205f10fca7083
Shravani Allam Reddy <shravani.allam@gmail.com>
2020-11-11 11:46:52 -0700

    updated connection to jawsdb on heroku

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,13 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
-  const connection = mysql.createConnection({
+   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "Sweety123",
     database: "burgers_db"
-  });
+  })
 }
 
 // Make connection.
